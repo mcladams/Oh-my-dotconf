3c799a6680653a1cb3b5abe25b5135dee0859575
Michael C. Adams <10792116+mcladams@users.noreply.github.com>
2023-09-09 15:20:56 +0800

    inc modified files

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,19 @@
-
+# powershell profile for dotconf git
 try { $null = gcm pshazz -ea stop; pshazz init } catch { }
+
 function conf { git --git-dir=$HOME\.conf.git --work-tree=$HOME $args }
+
+if (Get-Command scoop 2> $null) {
+  $doUpdate = $true;
+  if ($lastupdate = Get-Content "$env:LOCALAPPDATA\last-scoop-update-timestamp" 2> $null) {
+    if ( (Get-Date -UnixTimeSeconds ([int]$lastupdate + 7 * 24 * 60)) -gt (Get-Date) ) {
+      $doUpdate = $false
+    }
+  }
+  if ($doUpdate) {
+    scoop update;
+    sudo scoop update -a -g;
+    sudo scoop cleanup -a -g;
+    Get-Date -UFormat "%s" > "$env:LOCALAPPDATA\last-scoop-update-timestamp";
+  }
+}
--- install-1-scoop.ps1
+++ install-1-scoop.ps1
@@ -3,23 +3,32 @@ $env:SCOOP='SCOOP'
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP)
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
-# essentials
+
+# scoop install essentials
 scoop install 7zip git curl sudo
-sudo scoop install --globsal 7zip git curl
-# four levels of unix utils install in order or comment
-busybox
-gow 
-coreutils findutils
-uutils-coreutils
-# buckets
+sudo scoop install --global 7zip git curl
+
+# four levels of unix utils install in order or comment out
+# note: later scoop installs of a command overwrite previous shim
+scoop install busybox
+scoop install gow
+scoop install coreutils #findutils
+scoop install uutils-coreutils
+
+# scoop buckets
 scoop bucket add extras
 scoop bucket add versions
 scoop bucket add nerd-fonts
 scoop bucket add nonportable
-# tools/apps
+
+# linux utils
 scoop install aria2 git-with-openssh grep sed gawk
-install duf dust ripgrep lsd fd delta bat zoxide czkawka hyperfine helix
-scoop install CascadiaCode-NF JetBrainsMono-NF FiraCode-NF
+# rust apps
+scoop install duf dust ripgrep lsd fd delta bat zoxide czkawka hyperfine
+# fonts
+scoop install Cascadia-Code CascadiaCode-NF FiraCode FiraCode-NF 
+JetBrains-Mono JetBrainsMono
+
 # apps
 scoop install nano neovim kdiff3 vcredist2022 wsl-ssh-agent concfg 
 pshazz lxrunoffline
