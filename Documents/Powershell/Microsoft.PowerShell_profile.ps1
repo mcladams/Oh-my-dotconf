ce259e88c11c75dc6e8386416ba3c31a15781025
Michael C. Adams <10792116+mcladams@users.noreply.github.com>
2023-09-13 15:38:10 +0800

    profile, oh-my-posh in install script

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,13 @@
-# powershell profile for dotconf git
-try { $null = gcm pshazz -ea stop; pshazz init } catch { }
-
+# powershell profile for dotconf git; use 'conf' not 'git' in $HOME
 function conf { git --git-dir=$HOME\.conf.git --work-tree=$HOME $args }
 
+# aliases
+Set-Alias -Name npp -Value "$env:PROGRAMFILES\Notepad++\notepad++.exe"
+
+# something from pshazz module for loading ssh agent or keys
+try { $null = gcm pshazz -ea stop; pshazz init } catch { }
+
+# script to update scoop weekly
 if (Get-Command scoop 2> $null) {
   $doUpdate = $true;
   if ($lastupdate = Get-Content "$env:LOCALAPPDATA\last-scoop-update-timestamp" 2> $null) {
@@ -17,3 +22,10 @@ if (Get-Command scoop 2> $null) {
     Get-Date -UFormat "%s" > "$env:LOCALAPPDATA\last-scoop-update-timestamp";
   }
 }
+
+# check if Documents is a link and replace with target
+#if ( !$(Get-Item $env:USERPROFILE\Downloads).target) {
+#	$targ = $(echo $env:USERPROFILE\Downloads)
+#} else {
+#	$targ = $(Get-Item $env:USERPROFILE\Downloads).target
+#}
