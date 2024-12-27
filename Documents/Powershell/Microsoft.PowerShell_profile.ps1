c327747d5ce8a62db85ea6a3544e8ad2c81ab10e
Michael C. Adams <10792116+mcladams@users.noreply.github.com>
2024-12-27 17:25:18 +0800

    Modified $PROFILE and made some scripts to add.

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,7 @@
-# powershell profile for dotconf git; use 'conf' not 'git' in $env:USERPROFILE
-function conf { git --git-dir=$env:USERPROFILE\.conf.git --work-tree=$env:USERPROFILE $args }
+# powershell profile for dotconf git; use 'conf' not 'git' in $HOME
+function conf { git --git-dir="$HOME\.conf.git\" --work-tree=$HOME $args }
 
 # aliases
   Set-Alias -Name npp -Value "$env:PROGRAMFILES\Notepad++\notepad++.exe"
-  # prevent conf rm removing from working tree
+# prevent conf rm removing from working tree
   Set-Alias -Name "conf rm" -Value "conf rm --cached"
-
-# something from pshazz module for loading ssh agent or keys
-try { $null = gcm pshazz -ea stop; pshazz init } catch { }
-
-# script to update scoop weekly
-if (Get-Command scoop 2> $null) {
-  $doUpdate = $true;
-  $now = [int]$(Get-Date -UFormat %s);
-  if ($lastupdate = Get-Content "$env:LOCALAPPDATA\last-scoop-update-timestamp" 2> $null) {
-    if ( ([int]$lastupdate + (7 * 24 * 60 * 60)) -gt ($now) ) {
-      $doUpdate = $false
-    }
-  }
-  if ($doUpdate) {
-    scoop update;
-    sudo scoop update -a -g;
-    sudo scoop cleanup -a -g;
-    $now > "$env:LOCALAPPDATA\last-scoop-update-timestamp";
-  }
-}
